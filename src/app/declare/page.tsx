9f42a96d0f7f0ed924a10fe62739ba5fc59cef0f
thomasisebeck <tisebeck@proton.me>
2024-05-14 17:41:57 +0200

    did statbox and set up fonts

--- src/app/declare/page.tsx
+++ src/app/declare/page.tsx
@@ -1,14 +1,17 @@
-import s from './declare.module.scss';
+import Statbox from "@/app/declare/statbox/statbox";
+import s from './delclare.module.scss'
 
-export default function Declare() {
+export default function Declare(props: { name: string, taxRate: number , year: number, universeFunds: number}) {
     return (
-        <div className={s.stats}>
+        <div className={s.container}>
+            <Statbox name={props.name} taxRate={props.taxRate} universeFunds={props.universeFunds} year={props.year}/>
             <div>
-                <div>
-                    <div>Income received</div>
-                    <input type={"text"}/>
-                </div>
+                <p>What is this year's tax rate going to be</p>
+                <input type={"text"} />
+                <div className={s.buttons}></div>
+                <button>Confirm</button>
+                <button>Cancel</button>
             </div>
         </div>
-    );
+    )
 }
