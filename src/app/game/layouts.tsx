c16fad2f06f17993841bfb897dfe00fca1b6f09c
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-12 16:24:20 +0200

    Added Mobile Layout

--- src/app/game/layouts.tsx
+++ src/app/game/layouts.tsx
@@ -16,6 +16,88 @@ const MatchMedia = dynamic(async() => {
 
 const Layouts = forwardRef(function Layouts({}, ref:Ref<any>) {
 
+	let gameContent = (
+		<GameContentContainer>
+			<AnimationContainer 
+				className="opacity-0" 
+				enter={{
+					animations:{
+						opacity: [0,1],
+						y: [-100, 0],
+					},
+					options:{
+						duration: 0.5,
+						ease: "circInOut",
+						delay: 0.0,
+					}
+				}}
+			>
+				<GameContent className="absolute self-end m-2">
+					<div className="min-w-24 aspect-square bg-green-200"></div>
+				</GameContent>
+			</AnimationContainer>
+			<AnimationContainer 
+				className="opacity-0" 
+				enter={{
+					animations:{
+						opacity: [0,1],
+						y: [-100, 0],
+					},
+					options:{
+						duration: 0.5,
+						ease: "circInOut",
+						delay: 0.1,
+					}
+				}}
+			>
+				<GameContent className="absolute self-center m-2">
+					<div className="min-w-24 aspect-square bg-blue-200"></div>
+				</GameContent>
+			</AnimationContainer>
+			<AnimationContainer 
+				className="opacity-0" 
+				enter={{
+					animations:{
+						opacity: [0,1],
+						y: [-100, 0],
+					},
+					options:{
+						duration: 0.5,
+						ease: "circInOut",
+						delay: 0.2,
+					}
+				}}
+			>
+				<GameContent className="absolute self-start m-2">
+					<div className="min-w-24 aspect-square bg-red-200"></div>
+				</GameContent>
+			</AnimationContainer>
+		</GameContentContainer>
+	)
+
+	const MobileLayout = forwardRef(function Layout({}, ref:Ref<any>) {
+		return(
+			<div ref={ref} className="max-h-svh max-w-svw h-screen w-screen overflow-hidden bg-white text-white grid grid-rows-12 gap-2 p-2">
+				<main className="row-span-11 bg-black rounded-md flex p-2 mb-8 relative justify-center items-center">
+					<ContentLayersContainer>
+						<ContentLayer>
+							<GameHUD>
+								<WorldHUD/>
+								<PlayerHUD/>
+							</GameHUD>
+						</ContentLayer>
+
+						<ContentLayer>
+							{gameContent}
+						</ContentLayer>
+
+					</ContentLayersContainer>
+				</main>
+				<GameFooter/>
+			</div>
+		);
+	});
+
 	const PCLayout = forwardRef(function Layout({}, ref:Ref<any>) {
 		return(
 			<div ref={ref} className="max-h-svh max-w-svw h-screen w-screen overflow-hidden bg-white text-white grid grid-rows-12 gap-2 p-2">
@@ -29,62 +111,7 @@ const Layouts = forwardRef(function Layouts({}, ref:Ref<any>) {
 						</ContentLayer>
 
 						<ContentLayer>
-							<GameContentContainer>
-								<AnimationContainer 
-									className="opacity-0" 
-									enter={{
-										animations:{
-											opacity: [0,1],
-											y: [-100, 0],
-										},
-										options:{
-											duration: 0.5,
-											ease: "circInOut",
-											delay: 0.0,
-										}
-									}}
-								>
-									<GameContent className="absolute self-end m-2">
-										<div className="min-w-24 aspect-square bg-green-200"></div>
-									</GameContent>
-								</AnimationContainer>
-								<AnimationContainer 
-									className="opacity-0" 
-									enter={{
-										animations:{
-											opacity: [0,1],
-											y: [-100, 0],
-										},
-										options:{
-											duration: 0.5,
-											ease: "circInOut",
-											delay: 0.1,
-										}
-									}}
-								>
-									<GameContent className="absolute self-center m-2">
-										<div className="min-w-24 aspect-square bg-blue-200"></div>
-									</GameContent>
-								</AnimationContainer>
-								<AnimationContainer 
-									className="opacity-0" 
-									enter={{
-										animations:{
-											opacity: [0,1],
-											y: [-100, 0],
-										},
-										options:{
-											duration: 0.5,
-											ease: "circInOut",
-											delay: 0.2,
-										}
-									}}
-								>
-									<GameContent className="absolute self-start m-2">
-										<div className="min-w-24 aspect-square bg-red-200"></div>
-									</GameContent>
-								</AnimationContainer>
-							</GameContentContainer>
+							{gameContent}
 						</ContentLayer>
 
 					</ContentLayersContainer>
@@ -96,6 +123,12 @@ const Layouts = forwardRef(function Layouts({}, ref:Ref<any>) {
 
 	return (
 		<>
+			<MatchMedia 
+				query={["xs","sm"]}
+				hidingType="display"
+			>
+				<MobileLayout/>
+			</MatchMedia>
 			<MatchMedia 
 				query={{
 					"min-width":"lg"
