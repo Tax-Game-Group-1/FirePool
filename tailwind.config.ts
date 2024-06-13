b151a858c8eaba2ee001f4e7e60c08ed094039f3
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-13 13:03:13 +0200

    I accidentally removed the default breakpoints for tailwind, I put them back

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,16 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+	  screens: {
+		'xs': '210px',
+		'sm': '425px',
+		'hd': '1920px',
+		'2k': '2048px',
+		'4k': '3840px',
+		'8k': '7680px',
+	  },
     },
-	screens: {
-	  'xs': '210px',
-	  'sm': '425px',
-	  'hd': '1920px',
-	  '2k': '2048px',
-	  '4k': '3840px',
-	  '8k': '7680px',
-	  // => @media (min-width: 576px) { ... }
-	},
   },
   plugins: [],
 };
