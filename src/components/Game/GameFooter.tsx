915a5d3401f6c985bba8a58d93f593b1b0cc54bd
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-16 16:50:02 +0200

    Styled GameFooter

--- src/components/Game/GameFooter.tsx
+++ src/components/Game/GameFooter.tsx
@@ -3,8 +3,13 @@ import React, { forwardRef, MouseEvent, Ref, useEffect, useRef, useState } from 
 import dynamic from 'next/dynamic';
 
 import SVGIcon from '@/components/SVGIcon/SVGIcon'
-import { BackSquare, CurrencyIcon, ExitDoor, StatsIcon, PercentageIcon, CopyIcon, MenuIcon, InfoIcon } from "@/assets/svg/icons";
+import { BackSquare, CurrencyIcon, ExitDoor, StatsIcon, PercentageIcon, CopyIcon, MenuIcon, InfoIcon, SettingsIcon } from "@/assets/svg/icons";
 import { mergeRefs } from '@/mergeRefs/mergeRefs';
+
+import style from "./GameFooter.module.scss";
+import theme from "../../elements.module.scss";
+import { animate } from 'framer-motion';
+import { useSignalEvent } from '@catsums/signal-event-bus';
 
 const MatchMedia = dynamic(async() => {
 	let x = await import('@/components/useMediaQuery/useMediaQuery')
@@ -22,34 +27,59 @@ const GameFooter = forwardRef(function GameFooter(props,ref:Ref<any>) {
 
 
 	const Mobile = forwardRef(function Mobile(props,ref:Ref<any>){
-
-		let [popMenuOpen, setPopMenuOpen] = useState(false);
 		let popMenuBtnRef = useRef<any>();
 		let popMenuRef = useRef<any>();
 
-		useEffect(()=>{
-			let popUpBtn = popMenuBtnRef.current as HTMLDivElement;
-			popUpBtn?.classList.add(popMenuOpen ? "bg-white" : "bg-black");
-			popUpBtn?.classList.remove(popMenuOpen ? "bg-black" : "bg-white");
+		let popMenuOpen = useRef(false);
+
+		let toggleClasses = {
+			stroke: [theme.strokeSolidText, theme.strokeSolidElement],
+			fill: [theme.toolBar, theme.textBoxBackground],
+		}
+
+		function toggleOpen(){
+			popMenuOpen.current = !popMenuOpen.current;
 
 			let popMenu = popMenuRef.current as HTMLDivElement;
-			popMenuOpen ? popMenu?.classList.remove("hidden", "scale-y-0") : popMenu?.classList.add("hidden", "scale-y-0");
-			
-		})
+			let popUpBtn = popMenuBtnRef.current as HTMLDivElement;
+	
+			if(popMenuOpen.current){
+				popUpBtn?.classList.remove(toggleClasses.stroke[0]);
+				popUpBtn?.classList.add(toggleClasses.stroke[1]);
+				
+				popMenu?.classList.remove(style.popMenuClosed);
+	
+				popUpBtn?.classList.remove(toggleClasses.fill[0]);
+				popUpBtn?.classList.add(toggleClasses.fill[1]);
+	
+				animate(popMenu, { scaleY: [0,1], opacity: [0,1] }, {duration: 0.2, ease:"circInOut"});
+	
+			}else{
+				popUpBtn?.classList.add(toggleClasses.stroke[0]);
+				popUpBtn?.classList.remove(toggleClasses.stroke[1]);
+	
+				popUpBtn?.classList.add(toggleClasses.fill[0]);
+				popUpBtn?.classList.remove(toggleClasses.fill[1]);
+				animate(popMenu, { scaleY: [1,0], opacity: [1,0] }, {duration: 0.2, ease:"circInOut"}).then(()=>{
+					popMenu?.classList.add(style.popMenuClosed);
+				});
+			}
+
+		}
 
 		return (
-			<footer ref={ref} className="row-span-1 grid grid-cols-12 bg-black rounded-md p-0 px-2 gap-0 text-xs sm:text-sm">
-				<div className="rounded-md col-span-4 px-0 flex items-center justify-center">
-					<div className="bg-white text-black font-medium text-[0.5rem] sm:text-xs p-1 flex items-center rounded-md">
-						<span className="game-room-code inline-flex justify-center items-center p-0 m-0">{gameCode}</span>
+			<footer ref={ref} className={`${style.gameFooter} ${style.mobileGameFooter} ${theme.toolBar} ${theme.solidText}`}>
+				<div className={`${style.col} col-span-4 md:col-span-3 px-0`}>
+					<div className={`${style.roomCodeCont} ${theme.textBoxBackground} ${theme.inputText}`}>
+						<span className={`${style.roomCodeTxt}`}>{gameCode}</span>
 					</div>
-					<div className="px-2 w-10 h-full flex justify-center items-center">
-						<SVGIcon className="">
-							<CopyIcon fill="none" stroke="white"/>
+					<div className={`${style.roomCodeCopyCont} ${theme.strokeSolidText}`}>
+						<SVGIcon>
+							<CopyIcon fill="none"/>
 						</SVGIcon>
 					</div>
 				</div>
-				<div className="flex flex-col justify-center items-center rounded-md col-span-6">
+				<div className={`${style.col} col-span-6 md:col-span-4 flex-col`}>
 					<div>
 						<span className="">Game:</span>
 						<span className="game-name px-1">{gameName}</span>
@@ -59,22 +89,34 @@ const GameFooter = forwardRef(function GameFooter(props,ref:Ref<any>) {
 						<span className="host-name px-1">{hostName}</span>
 					</div>
 				</div>
-				<div className="flex flex-col justify-center items-center rounded-md col-span-2">
-					<div ref={popMenuRef} className="popmenu-menu absolute grid grid-flow-row bg-black border rounded-md w-20 p-4 bottom-[10%] gap-4 transition-all scale-y-0 origin-bottom hidden">
+				<div className={`justify-center items-center rounded-md hidden md:flex md:col-span-3 flex-col`}>
+					<div>
+						<span className="">Number of players:</span>
+					</div>
+					<div>
+						<span className="player-count px-1">{numOfPlayers}</span>
+						<span>/</span>
+						<span className="player-max-count px-1">{maxNumOfPlayers}</span>
+						
+					</div>
+				</div>
+				<div className={`${style.col} flex-col col-start-11 md:col-start-12`}>
+					<div ref={popMenuRef} className={`${style.popMenu} ${style.popMenuClosed} ${theme.toolBar}`}>
 						<div className="grid grid-flow-row">
-							<SVGIcon resizeBasedOnContainer={false}>
-								<InfoIcon fill="white"/>
-								<InfoIcon fill="white"/>
-								<InfoIcon fill="white"/>
+							<SVGIcon resizeBasedOnContainer={false} className={`${theme.fillSolidText} my-2`}>
+								<InfoIcon />
+								<StatsIcon />
+								<SettingsIcon />
+								<ExitDoor />
 							</SVGIcon>
 						</div>
 						<div className="popmenu-spacing"></div>
 					</div>
 					<div ref={popMenuBtnRef} onClick={()=>{
-						setPopMenuOpen(!popMenuOpen);
-					}} className="popmenu-icon flex absolute justify-center items-center rounded-full border w-14 p-3 bottom-[4.5%] aspect-square transition-all duration-300 bg-black">
-						<SVGIcon resizeBasedOnContainer={false}>
-							<MenuIcon fill="none" stroke={popMenuOpen ? "black" : "white"}/>
+						toggleOpen();
+					}} className={`${style.popMenuIcon} ${toggleClasses.stroke[0]} ${toggleClasses.fill[0]} ${theme.buttonBorder}`}>
+						<SVGIcon resizeBasedOnContainer={false} className={``}>
+							<MenuIcon/>
 						</SVGIcon>
 					</div>
 				</div>
@@ -84,38 +126,40 @@ const GameFooter = forwardRef(function GameFooter(props,ref:Ref<any>) {
 
 	const PC = forwardRef(function PC(props,ref:Ref<any>){
 		return (
-			<footer ref={ref} className="row-span-1 grid grid-cols-12 bg-black rounded-md p-0 md:p-1 gap-2 text-sm">
-				<div className="rounded-md col-span-3 px-2 flex items-center justify-center">
+			<footer ref={ref} className={`${style.gameFooter} ${style.PCGameFooter} ${theme.toolBar} ${theme.solidText}`}>
+				<div className={`${style.col} col-span-3 px-2`}>
 					<div className="mx-2">Game Code:</div>
-					<div className="bg-white text-black font-medium text-xs p-2 rounded-md">ABCD1234</div>
-					<div className="px-2 w-12 h-full flex justify-center items-center">
-						<SVGIcon className="">
-							<CopyIcon fill="none" stroke="white"/>
+					<div className={`${theme.textBoxBackground} ${theme.inputText} font-medium text-xs p-2 rounded-md`}>{gameCode}</div>
+					<div className={`${style.roomCodeCopyCont} ${theme.strokeSolidText}`}>
+						<SVGIcon>
+							<CopyIcon fill="none"/>
 						</SVGIcon>
 					</div>
 				</div>
-				<div className="flex justify-center items-center rounded-md col-span-3">
-					<span className="">Game:</span><span className="game-name px-1">Game 1</span>
+				<div className={`${style.col} col-span-3`}>
+					<span className="">Game:</span>
+					<span className="game-name px-1">{gameName}</span>
 				</div>
-				<div className="flex justify-center items-center rounded-md col-span-2">
+				<div className={`${style.col} col-span-2`}>
 					<span className="">Players:</span>
 					<span className="px-1">
-						<span className="player-count px-1">8</span>
-						/
-						<span className="player-max-count px-1">10</span>
+						<span className="player-count px-1">{numOfPlayers}</span>
+						<span>/</span>
+						<span className="player-max-count px-1">{maxNumOfPlayers}</span>
 					</span>
 				</div>
-				<div className="flex justify-center items-center rounded-md col-span-2">
-				<span className="">Host:</span><span className="host-name px-1">Dr F. Tax</span>
+				<div className={`${style.col} col-span-2`}>
+					<span className="">Host:</span>
+					<span className="host-name px-1">{hostName}</span>
 				</div>
-				<div className="flex relative justify-center items-center rounded-md col-start-11 px-2 w-12">
-					<SVGIcon className="">
-						<BackSquare fill='white'/>
+				<div className={`${style.col} ${theme.fillSolidText} col-start-11 px-2 w-12`}>
+					<SVGIcon>
+						<BackSquare/>
 					</SVGIcon>
 				</div>
-				<div className="flex justify-center items-center rounded-md col-start-12 px-2 w-12">
-					<SVGIcon className="">
-						<ExitDoor fill='white'/>
+				<div className={`${style.col} ${theme.fillSolidText} col-start-12 px-2 w-12`}>
+					<SVGIcon>
+						<ExitDoor/>
 					</SVGIcon>
 				</div>
 			</footer>
@@ -127,13 +171,12 @@ const GameFooter = forwardRef(function GameFooter(props,ref:Ref<any>) {
 			<MatchMedia 
 				query={{
 					not: ["xs","sm","md"]
-				}} hidingType="display"
+				}}
 			>
 				<PC/>
 			</MatchMedia>
 			<MatchMedia 
 				query={["xs","sm","md"]} 
-				hidingType="display"
 			>
 				<Mobile/>
 			</MatchMedia>
