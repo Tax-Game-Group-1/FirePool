ebc1c5eaa747a5ea0aef33df0042f33e009781b9
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-16 16:57:11 +0200

    Added Lazy loading to page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,10 +1,16 @@
 import React, { lazy, Suspense } from 'react'
 
 import {forwardRef, Ref} from 'react';
-import Layouts from './layouts';
 
-export default function Game() {
-  return (
-	<Layouts/>
-  )
+const Layouts = lazy(() => import("./layouts"))
+
+export default function Layout() {
+
+	return (
+		<div className={`w-full h-full flex justify-center items-center overflow-hidden`}>
+			<Suspense fallback={<p></p>}>
+				<Layouts/>
+			</Suspense>
+		</div>
+	)
 }
