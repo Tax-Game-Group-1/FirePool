2f87934622f2f53f82acbb49a72a0993c198c625
Cassim <111745304+catsums@users.noreply.github.com>
2024-06-18 16:45:01 +0200

    Checking for game code on game enter

--- src/app/game/layouts.tsx
+++ src/app/game/layouts.tsx
@@ -15,14 +15,22 @@ import LoadingStatus from '@/components/Loading/Loading';
 import InGame from './_content/InGame';
 import { createContent } from '@/components/Game/GameContentContainer';
 import TestContent1 from './_content/test1';
-import { signal } from '@preact/signals-react';
+import { signal, computed } from '@preact/signals-react';
 import { WaitingRoom } from './_waiting/waitingRoom';
 import { animate } from 'framer-motion';
-import { updateGameGlobal } from '@/app/global';
+import { GameGlobal, loadGameGlobal, updateGameGlobal } from '@/app/global';
 import LoadingScreen from '@/components/LoadingScreen/LoadingScreen';
 import { useRemoveLoadingScreen } from '@/components/LoadingScreen/LoadingScreenUtil';
+import { getData } from '../dummyData';
+import { createPopUp, popUpSignalBus } from '@/components/PopUp/PopUp';
+import { createNotif, notifSignalBus } from '@/components/Notification/Notification';
 
 let isLoaded = signal(false);
+
+export let playerID = computed(()=>{
+	let id = GameGlobal.playerData.value?.id || "";
+	return id;
+});
 
 enum GameScreen {
 	WaitingRoom,
@@ -36,13 +44,6 @@ const Layouts = forwardRef(function Layouts({}, ref:Ref<any>) {
 
 	let [gameScreen, setGameScreen] = useState(GameScreen.InGame);
 
-	useRemoveLoadingScreen(()=>{
-		isLoaded.value = true;
-	}),()=>{
-		let gameLayout = document.querySelector(`.${style.gameLayout}`);
-		gameLayout?.classList.remove("hidden");
-	};
-
 	useEffect(()=>{
 		window.scrollTo(0,0);
 		if(!isLoaded.value) return;
@@ -55,11 +56,67 @@ const Layouts = forwardRef(function Layouts({}, ref:Ref<any>) {
 		// 		useWrapper:false,
 		// 	})
 		// }, 1000)
-
-		return () => {
-			// clearTimeout(x);
+		function redirect(){
+			window.location.href = "/";
 		}
-	},[])
+
+		let url = new URL(window.location.href);
+		let params = url.searchParams;
+
+		let code = "";
+
+		if(params.has("c")){
+			code = params.get("c");
+		}
+		if(params.has("code")){
+			code = params.get("code");
+		}
+		
+		let roomData = getData("rooms", code);
+		if(!roomData){
+			let {timer} = createPopUp({
+				content: "This is an invalid room code. Redirecting you back...",
+				buttons:{
+					"okay": redirect,
+				},
+				onClose: redirect,
+			});
+			return () => {
+				timer.end();
+			}
+		}
+		if(!roomData.players.includes(playerID.value)){
+			let {timer} = createPopUp({
+				content: "You can't join this room because you did not join it properly. Redirecting you back...",
+				buttons:{
+					"okay": redirect,
+				},
+				onClose: redirect,
+			});
+			return () => {
+				timer.end();
+			}
+		}
+
+		GameGlobal.roomData.value = roomData;
+		updateGameGlobal();
+
+		let {timer,id} = createNotif({
+			content: "Joined game!",
+		});
+		return () => {
+			timer.end();
+		}
+	},[isLoaded.value])
+
+	useRemoveLoadingScreen(()=>{
+		isLoaded.value = true;
+		loadGameGlobal();
+	},()=>{
+		let gameLayout = document.querySelector(`.${style.gameLayout}`);
+		gameLayout?.classList.remove("hidden");
+	});
+	
 	
 	let content = (<></>)
 
