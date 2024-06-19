eb4a9f031e026743c8b8a4b57065e483241ffe7c
Catsim <111745304+catsums@users.noreply.github.com>
2024-06-19 21:41:19 +0200

    Added new icons

--- src/assets/svg/svg.tsx
+++ src/assets/svg/svg.tsx
@@ -503,3 +503,43 @@ export const MinimalMountains = forwardRef(function MinimalMountains({
   )
 })
 
+export const AddIcon = forwardRef(function AddIcon({
+	className="",fill="", stroke="", ...other
+}:ISVGProps, ref:Ref<any>){
+	return (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill={fill}
+		viewBox="0 0 24 24"
+		ref={ref}
+		stroke={stroke}
+		className={className}
+		{...other}
+	>
+		<path 
+			fillRule="evenodd" 
+			clipRule="evenodd" 
+			d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22ZM12 8.25C12.4142 8.25 12.75 8.58579 12.75 9V11.25H15C15.4142 11.25 15.75 11.5858 15.75 12C15.75 12.4142 15.4142 12.75 15 12.75H12.75L12.75 15C12.75 15.4142 12.4142 15.75 12 15.75C11.5858 15.75 11.25 15.4142 11.25 15V12.75H9C8.58579 12.75 8.25 12.4142 8.25 12C8.25 11.5858 8.58579 11.25 9 11.25H11.25L11.25 9C11.25 8.58579 11.5858 8.25 12 8.25Z" 
+		></path>
+  	</svg>
+  )
+})
+
+export const BinIcon = forwardRef(function BinIcon({
+	className="",fill="", stroke="", ...other
+}:ISVGProps, ref:Ref<any>){
+	return (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill={fill}
+		viewBox="0 0 24 24"
+		ref={ref}
+		stroke={stroke}
+		className={className}
+		{...other}
+	>
+		<path d="M5.755 20.283 4 8h16l-1.755 12.283A2 2 0 0 1 16.265 22h-8.53a2 2 0 0 1-1.98-1.717ZM21 4h-5V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v1H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2Z" />
+  	</svg>
+  )
+})
+
